3d3e22915e75d560976bc2ef9c74c361b7ec2336
b.elliottsmith <b.elliottsmith@localhost>
2010-02-26 19:29:36 +0000

    made many further improvements to the HashIter32Bit class, including optimising away most accesses to the resizingFrom[] array even when it *is* populated (which is an uncommon occurrence anyway), and introducing the capacity to concretely rule out ever re-visiting any hash that has previously been encountered by coupling the new visit() method with a hash table wherein all collisions are sorted by the value of Integer.reverse(hash) - THUS providing a concrete iteration order for a hash table that can be resized, or modified independently in anyway without invalidating the iterator, with only minimal memory requirements placed on the iterator.

--- jjoost-base/src/org/jjoost/collections/base/HashIter32Bit.java
+++ jjoost-base/src/org/jjoost/collections/base/HashIter32Bit.java
@@ -7,132 +7,165 @@ public class HashIter32Bit {
 	private final int lowBitsMask ;
 	private int numTotalBits ;
 	private int resizingTo ;
+	private int highBitsVisited ;
 	private int highBitsCounter ;
-	private int middleBitsCounter ;
+	private int highBitsIncrementor ;
 	private int lowBitsCounter ;
-	private int middleBitsIncrementor ;
+	
+	/**
+	 * saves the cursor position we were at prior to shrinking the number of hash bits used.
+	 * records are inserted at the beginning, and are deleted as soon as we reach the same
+	 * lowBitsCounter we were at when we began the shrink, after setting the highBitsVisited 
+	 * to the highBitsForEqualLowBits value of the deleted record; thus resizingFrom is:
+	 *     - sorted in ascending order of lowBits
+	 *         (because when we insert a record all entries must be by definition above the current lowBitsCounter value)
+	 *     - sorted in descending order of highBitsForLowerLowBits
+	 *         (because we can only insert these records in the case of a shrink, and so for a record to already be present
+	 *         it must have been inserted by a shrink still in progress which when instigated must have had a smaller highBitsIncrementor
+	 *         and, as such, for all lowBits that had completed prior to the shrink, the value of highBitsCounter + highBitsIncrementor 
+	 *         must be have been less than those completed since the shrink began)
+	 *     - TODO: prove that is sorted in ascending order of highBitsForHigherLowBits, which some of the logic requires
+	 */
 	private ResizingFrom[] resizingFrom = null ;
 	
 	private static final class ResizingFrom {
-		private final int highBitsForHigherLowBits ;
-		private final int highBitsForEqualLowBits ;
-		private final int highBitsForLowerLowBits ;
-		private final int lowBits ;
+		private int highBitsForEqualLowBits ;
+		private int highBitsForLowerLowBits ;
+		private int highBitsForHigherLowBits ;
+		private int lowBits ;
 		public ResizingFrom(int highBitsForEqualLowBits,
 				int highBitsForLowerLowBits, int highBitsForHigherLowBits, int lowBits) {
-			this.highBitsForHigherLowBits = highBitsForHigherLowBits;
 			this.highBitsForEqualLowBits = highBitsForEqualLowBits;
 			this.highBitsForLowerLowBits = highBitsForLowerLowBits;
+			this.highBitsForHigherLowBits = highBitsForHigherLowBits;
 			this.lowBits = lowBits;
 		}
 	}
 	
 	private void pushResizingFrom() {
-		if (resizingFrom == null) {
-			resizingFrom = new ResizingFrom[] { getResizingFrom() } ;
+		final ResizingFrom rf ;
+		if (lowBitsCounter == 0) {
+			rf = new ResizingFrom(highBitsCounter, -1, highBitsCounter, 1) ;
 		} else {
-			resizingFrom = Arrays.copyOf(resizingFrom, resizingFrom.length + 1) ;
-			resizingFrom[resizingFrom.length - 1] = getResizingFrom() ;
+			final int lowerLowBitsInc = (1 << (32 - numTotalBits)) ;
+			final int highBitsForLowerLowBits = (highBitsCounter & ~(lowerLowBitsInc - 1)) + lowerLowBitsInc ;
+			rf = new ResizingFrom(highBitsVisited, highBitsForLowerLowBits, highBitsCounter, lowBitsCounter) ;
 		}
-	}
-	
-	private void cleanResizingFrom() {
-		int removed = 0 ;
-		for (int i = 0 ; i != resizingFrom.length ; i++) {
-			if (highBitsCounter >= resizingFrom[i].highBitsForEqualLowBits) {
-				resizingFrom[i] = null ;
-				removed++ ;
-			}
+		if (resizingFrom == null) {
+			resizingFrom = new ResizingFrom[] { rf } ;
+		} else {
+			final ResizingFrom[] rfs = new ResizingFrom[resizingFrom.length + 1] ;
+			System.arraycopy(resizingFrom, 0, rfs, 1, resizingFrom.length) ;
+			resizingFrom[0] = rf ;
 		}
-		if (removed == resizingFrom.length) {
-			resizingFrom = null ;
-		} else if (removed != 0) {
-			ResizingFrom[] copy = new ResizingFrom[resizingFrom.length - removed] ;
-			int j = 0 ;
-			for (ResizingFrom sf : copy) {
-				if (sf != null) {
-					copy[j++] = sf ;
-				}
-			}
-			resizingFrom = copy ;
-		}
-	}
-	
-	private ResizingFrom getResizingFrom() {
-		return new ResizingFrom(highBitsCounter + (middleBitsCounter >>> numTotalBits), highBitsCounter + (1 << (32 - numTotalBits)), highBitsCounter, lowBitsCounter) ;
 	}
 	
 	public HashIter32Bit(int numLowerBits, int numTotalBits) {
 		this.numTotalBits = numTotalBits ;		
 		this.resizingTo = numTotalBits ;
 		this.lowBitsMask = (1 << numLowerBits) - 1 ;
+		this.highBitsIncrementor = 1 << (32 - numTotalBits) ;
 	}
 	
-	public void resize(int numTotalBits) {
-		if (numTotalBits > this.numTotalBits) {
-			resizingTo = numTotalBits ;
-			middleBitsIncrementor = 1 << (32 - (numTotalBits - this.numTotalBits)) ;
-			if ((middleBitsCounter & middleBitsIncrementor) != middleBitsCounter)
-				pushResizingFrom() ;
-			middleBitsCounter &= middleBitsIncrementor ;
-		} else if (numTotalBits < this.numTotalBits) {
-			final int newHighBitsCounter = highBitsCounter & ~((1 << (32 - numTotalBits)) - 1) ;
-			if ((lowBitsCounter != 0) || (middleBitsCounter != 0) || highBitsCounter != newHighBitsCounter)
-				pushResizingFrom() ;
-			this.numTotalBits = numTotalBits ;
-			resizingTo = numTotalBits ;
-			middleBitsIncrementor = 0 ;
-			middleBitsCounter = 0 ;
+	public void resize(int newTotalBits) {
+		final int newHighBitsIncrementor = 1 << (32 - newTotalBits) ;
+		// quite neatly we can use the highBitsVisited here to obtain the newHighBitsCounter in all possible resizing scenarios, despite their effects being quite different
+		final int newHighBitsCounter = highBitsVisited & ~(newHighBitsIncrementor - 1) ;
+		if (newTotalBits > this.numTotalBits) {
+			resizingTo = newTotalBits ;
+			highBitsIncrementor = newHighBitsIncrementor ;
 			highBitsCounter = newHighBitsCounter ;
-			lowBitsCounter = 0 ;
+		} else if (newTotalBits < this.numTotalBits) {
+			if (lowBitsCounter == 0) {
+				// insert a resizing from record IFF we are losing information about how far we have traversed for future records
+				if ((resizingFrom == null || resizingFrom[0].lowBits > 1) && (highBitsCounter & ~(newHighBitsIncrementor - 1)) < highBitsCounter)
+					pushResizingFrom() ;
+			} else {
+				pushResizingFrom() ;
+				lowBitsCounter = 0 ;
+				highBitsVisited = newHighBitsCounter ;
+			}
+			numTotalBits = newTotalBits ;
+			resizingTo = newTotalBits ;
+			highBitsIncrementor = newHighBitsIncrementor ;
+			highBitsCounter = newHighBitsCounter ;
 		}
 	}
 	
 	public boolean next() {
-		middleBitsCounter = middleBitsCounter + middleBitsIncrementor ;
-		if (middleBitsCounter == 0) {
+		final int highBitsMask = (1 << (32 - numTotalBits)) - 1 ;
+		highBitsCounter = highBitsCounter + highBitsIncrementor ;
+		if ((highBitsCounter & highBitsMask) == 0) {
 			lowBitsCounter = (lowBitsCounter + 1) & lowBitsMask ;
-			if (lowBitsCounter == 0) {
-				highBitsCounter += 1 << (32 - numTotalBits) ;
-				middleBitsIncrementor = 0 ;
+			if (lowBitsCounter == 0) {				
+				highBitsIncrementor = 1 << (32 - resizingTo) ;
 				numTotalBits = resizingTo ;
-				if (resizingFrom != null)
-					cleanResizingFrom() ;
 				if (highBitsCounter == 1 << (32 - Integer.bitCount(lowBitsMask)))
 					return false ;
+			} else {
+				highBitsCounter -= highBitsMask + 1;
 			}
+			highBitsVisited = highBitsCounter ;
+			ResizingFrom rf ;
+			if (resizingFrom != null && (rf = resizingFrom[0]).lowBits == lowBitsCounter) {
+				if (highBitsVisited < rf.highBitsForEqualLowBits)
+					highBitsVisited = rf.highBitsForEqualLowBits ;				
+				if (rf.highBitsForHigherLowBits > highBitsCounter && (resizingFrom.length == 1 || resizingFrom[1].lowBits == rf.lowBits + 1)) {
+					rf.lowBits++ ;
+					rf.highBitsForLowerLowBits = -1 ;
+					rf.highBitsForEqualLowBits = rf.highBitsForHigherLowBits ;
+				} else {
+					if (resizingFrom.length == 1) resizingFrom = null ;
+					else resizingFrom = Arrays.copyOfRange(resizingFrom, 1, resizingFrom.length) ;
+				}
+			}
+		} else {
+			highBitsVisited = highBitsCounter ;
 		}
 		return true ;
+	}
+	
+	public boolean visit(int hash) {
+		final int highBits = Integer.reverse(hash & ~lowBitsMask) ;
+		final int lowBits = hash & lowBitsMask ;
+		if (lowBits == lowBitsCounter) {
+			if (highBitsVisited > highBits)
+				return false ;
+			highBitsVisited = highBits ;
+			return true ;
+		} else {
+			throw new IllegalArgumentException(String.format("The provided hash should not have occured at index %d of a " +
+					"hash array of size %d, so this method is being used incorrectly, or there is a problem with the hash table", 
+					current(), 1 << numTotalBits)) ;
+		}
 	}
 	
 	public boolean haveVisitedAlready(int hash) {
 		final int highBits = Integer.reverse(hash & ~lowBitsMask) ;
 		final int lowBits = hash & lowBitsMask ;
-		int cmpHighBits ;
 		if (lowBits < lowBitsCounter) {
-			cmpHighBits = highBitsCounter + (1 << (32 - numTotalBits)) ;
+			return highBitsCounter + (1 << (32 - numTotalBits)) > highBits ;
 		} else if (lowBits == lowBitsCounter) {
-			cmpHighBits = highBitsCounter + (middleBitsCounter >>> numTotalBits) ;
+			return highBitsVisited > highBits ;
 		} else {
-			cmpHighBits = highBitsCounter ;
+			if (highBitsCounter > highBits)
+				return true ;
+			if (resizingFrom != null) { 
+				for (ResizingFrom rf : resizingFrom) {
+					if (lowBits < rf.lowBits)
+						return rf.highBitsForLowerLowBits > highBits ;
+					else if (lowBits == rf.lowBits)
+						return rf.highBitsForEqualLowBits > highBits ;
+					else if (rf.highBitsForHigherLowBits > highBits)
+						return true ;
+				}
+			}
+			return false ;
 		}
-		if (resizingFrom != null) {
-			for (ResizingFrom rf : resizingFrom) {
-				if (lowBits < rf.lowBits) {
-					if (cmpHighBits < rf.highBitsForEqualLowBits)
-						cmpHighBits = rf.highBitsForEqualLowBits ;
-				} else if (lowBits == rf.lowBits) {
-					if (cmpHighBits < rf.highBitsForLowerLowBits)
-						cmpHighBits = rf.highBitsForLowerLowBits ;
-				} else if (cmpHighBits < rf.highBitsForHigherLowBits)
-					cmpHighBits = rf.highBitsForHigherLowBits ;
-			}		
-		} 
-		return cmpHighBits > highBits ;
 	}
-	
+
 	public int current() {
-		return Integer.reverse(highBitsCounter + (middleBitsCounter >>> numTotalBits)) | lowBitsCounter ;
+		return Integer.reverse(highBitsCounter) | lowBitsCounter ;
 	}
 
 }
