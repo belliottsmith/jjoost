599ae6a7d7e2afffa07a9510d47fb8c221f33062
b.elliottsmith <b.elliottsmith@localhost>
2010-03-28 20:48:49 +0100

    moved insertBefore() methods from SerialHashStore to HashNode

--- jjoost-base/src/org/jjoost/collections/base/HashNode.java
+++ jjoost-base/src/org/jjoost/collections/base/HashNode.java
@@ -3,6 +3,7 @@ package org.jjoost.collections.base ;
 import java.io.Serializable ;
 
 public abstract class HashNode<N extends HashNode<N>> implements Serializable {
+	
 	private static final long serialVersionUID = 2035712133283347382L;
 	public final int hash ;
 	public HashNode(int hash) {
@@ -10,5 +11,17 @@ public abstract class HashNode<N extends HashNode<N>> implements Serializable {
 	}
 	public abstract N copy() ;
 	public final int hash() { return hash ; }
+	
+	public static boolean insertBefore(int reverseHash, HashNode<?> node) {
+		final int reverseNodeHash = Integer.reverse(node.hash) ; 
+		return (reverseHash < reverseNodeHash) ^ ((reverseNodeHash > 0) != (reverseHash > 0));
+	}
+
+	public static boolean insertBefore(HashNode<?> a, HashNode<?> b) {
+		final int ra = Integer.reverse(a.hash) ; 
+		final int rb = Integer.reverse(b.hash) ; 
+		return (ra < rb) ^ ((ra > 0) != (rb > 0));
+	}
+	
 }
 
