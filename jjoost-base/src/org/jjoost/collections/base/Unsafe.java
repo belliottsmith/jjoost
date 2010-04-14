5295d5561cda8e64fb3df38447488e7791e16c5c
b.elliottsmith <b.elliottsmith@localhost>
2010-04-14 20:54:50 +0100

    moved Unsafe methods to internal Unsafe class for ease

---
+++ jjoost-base/src/org/jjoost/collections/base/Unsafe.java
@@ -0,0 +1,41 @@
+package org.jjoost.collections.base;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.UndeclaredThrowableException;
+
+@SuppressWarnings("restriction")
+class Unsafe {
+
+	static final sun.misc.Unsafe INST = getUnsafe() ;
+	
+	private static final sun.misc.Unsafe getUnsafe() {
+		
+		sun.misc.Unsafe unsafe = null ;
+		try {
+			Class<?> uc = sun.misc.Unsafe.class;
+			Field[] fields = uc.getDeclaredFields();
+			for (int i = 0; i < fields.length; i++) {
+				if (fields[i].getName().equals("theUnsafe")) {
+					fields[i].setAccessible(true);
+					unsafe = (sun.misc.Unsafe) fields[i].get(uc);
+					break;
+				}
+			}
+		} catch (Exception e) {
+			throw new UndeclaredThrowableException(e) ;
+		}
+		
+		return unsafe;
+	}
+	
+	public static long fieldOffset(Class<?> clazz, String field) {
+		try {
+			final Field f = clazz.getDeclaredField(field) ;
+			return Unsafe.INST.objectFieldOffset(f) ;
+		} catch (Exception e) {
+			throw new UndeclaredThrowableException(e) ;
+		}
+
+	}
+
+}
